afc7d18abc7895d550c59e907cf8a4673e0c50b2
coldpigli <piyushdas98@gmail.com>
2021-09-03 22:53:24 +0530

    completed stonks

--- src/App.js
+++ src/App.js
@@ -2,42 +2,68 @@ import React, { useState } from 'react';
 import Alert from './Alert';
 function App() {
   
-  const[initialRate, setInitialRate] = useState();
-  const[quantity, setQuantity] = useState();
-  const[currentRate, setCurrentRate] = useState();
+  //defining states
+  const[initialRate, setInitialRate] = useState('');
+  const[quantity, setQuantity] = useState('');
+  const[currentRate, setCurrentRate] = useState('');
   const[alertMessage, setAlertMessage] = useState('');
   const[gifSelect, setGifSelect] = useState('');
   
 
+  //function to calculate profit
   const calculateProfit = () => {
     let profitPerShare = Number(currentRate) - Number(initialRate);
     let profit = profitPerShare * Number(quantity);
     let profitPercentage = (profitPerShare/initialRate) * 100;
-    setAlertMessage("To the moon!! You made a profit of "+ profitPercentage + "% evaluating to "+ profit);
+    setAlertMessage("To the moon!! You made a profit of "+ profitPercentage.toFixed(2) + "% evaluating to "+ profit.toFixed(2));
     setGifSelect('profit');
   }
 
+  //function to calculate Loss
   const calculateLoss = () => {
     let lossPerShare = Number(initialRate) - Number(currentRate);
     let loss = lossPerShare * Number(quantity);
     let lossPercentage = (lossPerShare/initialRate) * 100;
-    setAlertMessage("Sorry!! You made a loss of "+ lossPercentage + "% evaluating to "+ loss);
+    setAlertMessage("Sorry!! You made a loss of "+ lossPercentage.toFixed(2) + "% evaluating to "+ loss.toFixed(2));
     setGifSelect('loss');
   }
 
-  const calculateResult = () => {
-    if(Number(initialRate)===Number(currentRate)){
-      setAlertMessage("You're breaking even!");
-      setGifSelect('even');
+  //function for validation
+  const validationFunction = () => {
+    if(initialRate === '' || quantity === '' || currentRate === ''){
+      setAlertMessage("Inital Rate / Quantity of Stocks / Current Rate cannot be empty");
+      return false;
     }
-    if(Number(initialRate)<Number(currentRate)){
-      calculateProfit();
+    else{
+      if(Number(initialRate) <= 0 || Number(quantity) <=0){
+        setAlertMessage("Initial rate OR quantity cannot be zero or less than zero");
+        return false;
+      }
     }
-    if(Number(initialRate)>Number(currentRate)){
-      calculateLoss();
-    }
+    return true;
   }
 
+  //function to calc profit/loss
+  const calculateResult = () => {
+    if(validationFunction()){
+      if(Number(initialRate)===Number(currentRate)){
+        setAlertMessage("You're breaking even!");
+        setGifSelect('even');
+      }
+      if(Number(initialRate)<Number(currentRate)){
+        calculateProfit();
+      }
+      if(Number(initialRate)>Number(currentRate)){
+        calculateLoss();
+      }
+    }
+    else{
+      return;
+    }
+    
+  }
+
+  
 
   
   return (
@@ -45,7 +71,7 @@ function App() {
     <h1>Keep an Eye on Your Investments!</h1>
     <label htmlFor="initialPrice">Initial Price</label>
     <input id = "initialPrice" type = "number" placeholder="Enter initial stock price here"
-     onChange = {(e)=>{setInitialRate(e.target.value)}}></input>
+     onChange = {(e)=>{setInitialRate(e.target.value)}} ></input>
     <br/>
     <label htmlFor="quantity">Enter number of stocks</label>
     <input id = "quantity" type = "number" placeholder="Enter the number of stocks"
@@ -61,7 +87,8 @@ function App() {
       <br/>
       <Alert gifSelect={gifSelect}/>
     </div>
-    <button onClick = {calculateResult} >Calculate</button>
+    <button onClick = {()=>{
+      calculateResult();}} >Calculate</button>
   </div>
     )
 }
